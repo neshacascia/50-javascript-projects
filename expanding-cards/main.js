89f3136da5fe9564a94b853481dc75edcb6f8afa
Nesha Mervin <nesha.mervin@gmail.com>
2022-11-07 15:49:35 -0500

    added more pseudo code

--- expanding-cards/main.js
+++ expanding-cards/main.js
@@ -1,17 +1,17 @@
 //grab all the images and store them as an array
 const panels = document.querySelectorAll('.panel');
 
-//loop through the array, adding an event listener:
-panels.forEach((panel) => {
+//loop through the array, adding an event listener to each image:
+panels.forEach(panel => {
   panel.addEventListener('click', function () {
-    //when each panel is clicked, we call the function to remove the active class on all other panels and add the active class on the panel that was clicked
+    //when each panel is clicked, we call the function to first remove the active class on all other panels and then add the active class on the panel that was clicked
     removeActiveClasses();
     panel.classList.add('active');
   });
 });
 
 function removeActiveClasses() {
-  panels.forEach((panel) => {
+  panels.forEach(panel => {
     panel.classList.remove('active');
     //removes all the active classes
   });
