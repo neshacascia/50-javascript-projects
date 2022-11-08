5a331dc03fad6e39a86b9942200e1b5757a1ca6f
Nesha Mervin <nesha.mervin@gmail.com>
2022-11-08 00:36:38 -0500

    added more pseudo code

--- progress-steps/main.js
+++ progress-steps/main.js
@@ -3,13 +3,13 @@ const prev = document.querySelector('#prev');
 const next = document.querySelector('#next');
 const circles = document.querySelectorAll('.circle');
 
-//this represents whichever circle is active which is set to 1 by default:
+//this represents whichever circle is currently active, we set it to the first circle by default:
 let currentActive = 1;
 
 next.addEventListener('click', () => {
   currentActive++;
 
-  //we do not want currentActive to be greater than 4
+  //we do not want currentActive to be no more than 4
   if (currentActive > circles.length) {
     currentActive = circles.length;
   }
@@ -19,6 +19,7 @@ next.addEventListener('click', () => {
 prev.addEventListener('click', () => {
   currentActive--;
 
+  //in case currentActive falls below 1, we reset it back to 1
   if (currentActive < 1) {
     currentActive = 1;
   }
@@ -28,24 +29,31 @@ prev.addEventListener('click', () => {
 
 function update() {
   circles.forEach((circle, index) => {
-    //since currentActive will always be ahead of index by 1 when we click next button, each circle will get added the active class
+    //since currentActive will always be ahead of our index by 1 when we click the next button, each circle will get added the active class
     if (index < currentActive) {
       circle.classList.add('active');
+
+      // once we start clicking the previous button, index will be greater than currentActive, each circle will lose the active class
     } else {
       circle.classList.remove('active');
     }
   });
 
+  // grabs all the circles with active class
   const actives = document.querySelectorAll('.active');
 
-  progress.style.width =
-    ((actives.length - 1) / (circles.length - 1)) * 100 + '%';
+  // increases the width of the progress by 25%
+  progress.style.width = (actives.length / 0.25) * 100 + '%';
 
+  // if currentActive is 1, there are no circles before
   if (currentActive === 1) {
     prev.disabled = true;
+
+    // if currentActive is 4, it's at the max, there are no circles after
   } else if (currentActive === circles.length) {
     next.disabled = true;
   } else {
+    // otherwise do not disable the prev or next button
     prev.disabled = false;
     next.disabled = false;
   }
